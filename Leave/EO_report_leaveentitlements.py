f4234a8590a8a01a33d71fdb4007c71709e711d5
eng <33153508+enggar10@users.noreply.github.com>
2022-08-03 17:38:46 +0700

    Add files via upload

---
+++ Leave/EO_report_leaveentitlements.py
@@ -0,0 +1,104 @@
+#from dbm.ndbm import library
+import dbm
+from tarfile import PAX_NAME_FIELDS
+import unittest
+import time
+from urllib import response
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+#variable
+url="https://opensource-demo.orangehrmlive.com/"
+username="Admin"
+password="admin123"
+title="manager"
+desk="mengerjakan semua tugas pegawai"
+note="isilah"
+
+#driver = webdriver.Chrome(executable_path="C:\Users\Enggar\python\chromedriver.exe")
+
+
+
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        
+    
+    #test case pertama
+    def test_success_login(self):
+
+        driver = self.driver
+        driver.get(url) # buka situs
+        time.sleep(3)
+        driver.find_element(By.NAME,"txtUsername").send_keys(username) # isi username
+        time.sleep(1)
+        driver.find_element(By.NAME,"txtPassword").send_keys(password) # isi password
+        time.sleep(5)
+        driver.find_element(By.NAME,"Submit").click()
+        time.sleep(5)
+
+        self.driver.implicitly_wait(0.5)
+        a = ActionChains(self.driver)
+        m = driver.find_element(By.ID,"menu_leave_viewLeaveModule")
+        a.move_to_element(m).perform()
+        #identify sub menu element
+        n = driver.find_element(By.ID,"menu_leave_Reports")
+        a.move_to_element(n).perform()
+        o = driver.find_element(By.ID,"menu_leave_viewLeaveBalanceReport")
+        #hover over element and click
+        a.move_to_element(o).click().perform()
+        time.sleep(2)
+
+        select_element = driver.find_element(By.ID,"leave_balance_report_type") #select dropdown
+        select_object = Select (select_element)
+        select_object.select_by_value('1')
+        time.sleep(2)
+
+        select_element = driver.find_element(By.ID,"leave_balance_leave_type") #select dropdown
+        select_object = Select (select_element)
+        select_object.select_by_value('10')
+        time.sleep(2)
+
+        select_element = driver.find_element(By.ID,"period") #select dropdown
+        select_object = Select (select_element)
+        select_object.select_by_value('2021-01-01$$2021-12-31')
+        time.sleep(2)
+
+        select_element = driver.find_element(By.ID,"leave_balance_job_title") #select dropdown
+        select_object = Select (select_element)
+        select_object.select_by_value('0')
+        time.sleep(2)
+
+        select_element = driver.find_element(By.ID,"leave_balance_location") #select dropdown
+        select_object = Select (select_element)
+        select_object.select_by_value('-1')
+        time.sleep(2)
+
+        select_element = driver.find_element(By.ID,"leave_balance_sub_unit") #select dropdown
+        select_object = Select (select_element)
+        select_object.select_by_value('0')
+        time.sleep(2)        
+
+        driver.find_element(By.ID,"leave_balance_include_terminated").click()
+        time.sleep(2)
+
+
+        driver.find_element(By.ID,"viewBtn").click()
+        time.sleep(3)
+
+         
+
+    def tearDown(self): 
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()   
